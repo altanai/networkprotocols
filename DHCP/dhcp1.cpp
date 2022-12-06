615012eb8d722c5c82e1e91385ddcf64b1197076
altanai <tara181989@gmail.com>
2022-12-06 11:42:58 -0800

    work in progress

--- DHCP/dhcp1.cpp
+++ DHCP/dhcp1.cpp
@@ -1,6 +1,6 @@
 // dhcp1.cpp
 
-//https://stackoverflow.com/questions/17866222/dhcp-c-program
+// https://stackoverflow.com/questions/17866222/dhcp-c-program
 
 #include "StdAfx.h"
 #include "WINDOWS.H"
@@ -10,10 +10,9 @@
 
 using namespace std;
 
-#pragma comment( lib, "dhcpcsvc.lib" )
+#pragma comment(lib, "dhcpcsvc.lib")
 
 #define NUMBER_OF_OPTIONS 7
-
 
 //#define NUMBER_OF_OPTIONS 4
 enum OptionArrayPositions
@@ -45,124 +44,116 @@ BOOL RetrieveDHCPInfo(LPWSTR pszAdapterName)
    DWORD dwError, dwSize;
    CHAR TmpBuffer[1000]; // host name won't be larger than this
 
-
    DHCPCAPI_PARAMS DhcpApiMaskParams = {0,
-                                    OPTION_SUBNET_MASK,
-                                    FALSE,
-                                    NULL,
-                                    0
-                                    };
+                                        OPTION_SUBNET_MASK,
+                                        FALSE,
+                                        NULL,
+                                        0};
    DHCPCAPI_PARAMS DhcpApiDNSParams = {0,
-                                    OPTION_DOMAIN_NAME_SERVERS,
-                                    FALSE,
-                                    NULL,
-                                    0
-                                    };
-   DHCPCAPI_PARAMS DhcpApiHostNameParams = {0,                // Flags
-                                       OPTION_HOST_NAME, // OptionId
-                                       FALSE,            // vendor specific?
-                                       NULL,             // data filled in on return
-                                       0                 // nBytes
-                                       }; 
-   DHCPCAPI_PARAMS DhcpApiVendorParams = {0,
-                                       OPTION_VENDOR_SPEC_INFO,
+                                       OPTION_DOMAIN_NAME_SERVERS,
                                        FALSE,
                                        NULL,
-                                       0
-                                    };
-   DHCPCAPI_PARAMS DhcpApiLeaseTimeParams = {0,
-                                          OPTION_LEASE_TIME,
+                                       0};
+   DHCPCAPI_PARAMS DhcpApiHostNameParams = {
+       0,                // Flags
+       OPTION_HOST_NAME, // OptionId
+       FALSE,            // vendor specific?
+       NULL,             // data filled in on return
+       0                 // nBytes
+   };
+   DHCPCAPI_PARAMS DhcpApiVendorParams = {0,
+                                          OPTION_VENDOR_SPEC_INFO,
                                           FALSE,
                                           NULL,
-                                          0
-                                       };    
+                                          0};
+   DHCPCAPI_PARAMS DhcpApiLeaseTimeParams = {0,
+                                             OPTION_LEASE_TIME,
+                                             FALSE,
+                                             NULL,
+                                             0};
    DHCPCAPI_PARAMS DhcpApiMsgTypeParams = {0,
-                                       OPTION_MESSAGE_TYPE,
-                                       FALSE,
-                                       NULL,
-                                       0
-                                       };
+                                           OPTION_MESSAGE_TYPE,
+                                           FALSE,
+                                           NULL,
+                                           0};
 
    DHCPCAPI_PARAMS DhcpApiServerNameParams = {0,
-                                          OPTION_SERVER_IDENTIFIER,
-                                          FALSE,
-                                          NULL,
-                                          0
-                                          };
+                                              OPTION_SERVER_IDENTIFIER,
+                                              FALSE,
+                                              NULL,
+                                              0};
 
-   DHCPCAPI_PARAMS paramArray[NUMBER_OF_OPTIONS] = { DhcpApiMaskParams, 
-                                                DhcpApiDNSParams,
-                                                DhcpApiHostNameParams, 
-                                                DhcpApiVendorParams, 
-                                                DhcpApiLeaseTimeParams,
-                                                DhcpApiMsgTypeParams,
-                                                DhcpApiServerNameParams
-                                                };
+   DHCPCAPI_PARAMS paramArray[NUMBER_OF_OPTIONS] = {DhcpApiMaskParams,
+                                                    DhcpApiDNSParams,
+                                                    DhcpApiHostNameParams,
+                                                    DhcpApiVendorParams,
+                                                    DhcpApiLeaseTimeParams,
+                                                    DhcpApiMsgTypeParams,
+                                                    DhcpApiServerNameParams};
 
-   DHCPCAPI_PARAMS_ARRAY DhcpApiParamsArray = {NUMBER_OF_OPTIONS,  // only one option to request
-                                          paramArray
-                                          };
+   DHCPCAPI_PARAMS_ARRAY DhcpApiParamsArray = {NUMBER_OF_OPTIONS, // only one option to request
+                                               paramArray};
    DHCPCAPI_PARAMS_ARRAY SendParams = {0, NULL};
 
    dwSize = sizeof(TmpBuffer);
    dwError = DhcpRequestParams(DHCPCAPI_REQUEST_SYNCHRONOUS, // Flags
-                           NULL,                         // Reserved
-                           pszAdapterName,               // Adapter Name
-                           NULL,                         // not using class id
-                           SendParams,                   // nothing to send
-                           DhcpApiParamsArray,           // requesting params
-                           (PBYTE) TmpBuffer,            // buffer
-                           &dwSize,                      // buffer size
-                           NULL                          // Request ID
-                           );
+                               NULL,                         // Reserved
+                               pszAdapterName,               // Adapter Name
+                               NULL,                         // not using class id
+                               SendParams,                   // nothing to send
+                               DhcpApiParamsArray,           // requesting params
+                               (PBYTE)TmpBuffer,             // buffer
+                               &dwSize,                      // buffer size
+                               NULL                          // Request ID
+   );
 
    cout << "Your Error Code from DhcpRequestParams is " << dwError << endl;
-   if( ERROR_MORE_DATA == dwError ) 
+   if (ERROR_MORE_DATA == dwError)
    {
       //
       // dwSize is not large enough.
-       cout<<"dwSize is not large enough"<<endl;
+      cout << "dwSize is not large enough" << endl;
       //
    }
 
    int success = FALSE;
-   if( NO_ERROR == dwError ) 
+   if (NO_ERROR == dwError)
    {
-      for(int i=0; i<NUMBER_OF_OPTIONS; i++)
+      for (int i = 0; i < NUMBER_OF_OPTIONS; i++)
       {
-         if(paramArray[i].nBytesData != 0)
+         if (paramArray[i].nBytesData != 0)
          {
             success = true;
-            switch(paramArray[i].OptionId)
+            switch (paramArray[i].OptionId)
             {
-               case OPTION_SUBNET_MASK:
-                  PrintMask(paramArray[i]);
-                  break;
-               case OPTION_DOMAIN_NAME_SERVERS:
-                  PrintDNS(paramArray[i]);
-                  break;
-               case OPTION_HOST_NAME:
-                  PrintHostName(paramArray[i]);
-                  break;
-               case OPTION_VENDOR_SPEC_INFO:
-                  PrintVendor(paramArray[i]);
-                  break;
-               case OPTION_LEASE_TIME:
-                  PrintLeaseTime(paramArray[i]);
-                  break;
-               case OPTION_MESSAGE_TYPE:
-                  PrintMessageType(paramArray[i]);
-                  break;
-               case OPTION_SERVER_IDENTIFIER:
-                  PrintServerIP(paramArray[i]);
-                  break;
+            case OPTION_SUBNET_MASK:
+               PrintMask(paramArray[i]);
+               break;
+            case OPTION_DOMAIN_NAME_SERVERS:
+               PrintDNS(paramArray[i]);
+               break;
+            case OPTION_HOST_NAME:
+               PrintHostName(paramArray[i]);
+               break;
+            case OPTION_VENDOR_SPEC_INFO:
+               PrintVendor(paramArray[i]);
+               break;
+            case OPTION_LEASE_TIME:
+               PrintLeaseTime(paramArray[i]);
+               break;
+            case OPTION_MESSAGE_TYPE:
+               PrintMessageType(paramArray[i]);
+               break;
+            case OPTION_SERVER_IDENTIFIER:
+               PrintServerIP(paramArray[i]);
+               break;
             }
          }
       }
       cout << endl;
    }
 
-   if(success == TRUE)
+   if (success == TRUE)
       return TRUE;
    else
       return FALSE;
@@ -171,9 +162,9 @@ BOOL RetrieveDHCPInfo(LPWSTR pszAdapterName)
 void PrintAsAscii(DHCPCAPI_PARAMS param)
 {
    unsigned char temp[260];
-   CopyMemory(temp, 
-   param.Data,
-   param.nBytesData);
+   CopyMemory(temp,
+              param.Data,
+              param.nBytesData);
 
    temp[param.nBytesData] = '\0';
    cout << temp;
@@ -182,27 +173,27 @@ void PrintAsAscii(DHCPCAPI_PARAMS param)
 void PrintAsIP(DHCPCAPI_PARAMS param)
 {
    unsigned char temp[260];
-   CopyMemory(temp, 
-   param.Data,
-   param.nBytesData);
+   CopyMemory(temp,
+              param.Data,
+              param.nBytesData);
 
    temp[param.nBytesData] = '\0';
-   for(unsigned int i=0; i<param.nBytesData; i++)
-   { 
-      if(i%4 == 0)
+   for (unsigned int i = 0; i < param.nBytesData; i++)
+   {
+      if (i % 4 == 0)
       {
          cout << endl;
       }
-         cout << (int)temp[i]<< ".";
+      cout << (int)temp[i] << ".";
    }
 }
 
 void PrintAsInt(DHCPCAPI_PARAMS param)
 {
    unsigned char temp[260];
-   CopyMemory(temp, 
-   param.Data,
-   param.nBytesData);
+   CopyMemory(temp,
+              param.Data,
+              param.nBytesData);
 
    temp[param.nBytesData] = '\0';
    cout << (unsigned int)*temp;
@@ -211,11 +202,12 @@ void PrintAsInt(DHCPCAPI_PARAMS param)
 void PrintMask(DHCPCAPI_PARAMS param)
 {
    /* Print the subnet mask
-   * This is stored as Option 1
-   *
-   *
-   */
-   cout << endl << "MASK" << endl;
+    * This is stored as Option 1
+    *
+    *
+    */
+   cout << endl
+        << "MASK" << endl;
    cout << "--------------------------------------------";
    PrintAsIP(param);
 }
@@ -223,51 +215,58 @@ void PrintMask(DHCPCAPI_PARAMS param)
 void PrintDNS(DHCPCAPI_PARAMS param)
 {
    /* Print the DNS Servers
-   * This is stored as Option 6
-   *
-   *
-   */
-   cout << endl << "\nDNS SERVERS" << endl;
+    * This is stored as Option 6
+    *
+    *
+    */
+   cout << endl
+        << "\nDNS SERVERS" << endl;
    cout << "--------------------------------------------";
    PrintAsIP(param);
 }
 
 void PrintHostName(DHCPCAPI_PARAMS param)
 {
-   /* Print the Host name or IP address if 
-   * that's what's stored in this option
-   * This is stored as Option 12
-   *
-   */
-   cout << endl << "\nHOST NAME" << endl;
+   /* Print the Host name or IP address if
+    * that's what's stored in this option
+    * This is stored as Option 12
+    *
+    */
+   cout << endl
+        << "\nHOST NAME" << endl;
    cout << "--------------------------------------------";
-   cout << endl << "ANSI String" << endl;
+   cout << endl
+        << "ANSI String" << endl;
    PrintAsAscii(param);
-   cout << endl << "IP FORMAT";
+   cout << endl
+        << "IP FORMAT";
    PrintAsIP(param);
 }
 
 void PrintVendor(DHCPCAPI_PARAMS param)
 {
    /* Print the Vendor parameter
-   * This is stored as Option 43
-   *
-   *
-   */
-   cout << endl << "\nVENDOR PARAMETER" << endl;
+    * This is stored as Option 43
+    *
+    *
+    */
+   cout << endl
+        << "\nVENDOR PARAMETER" << endl;
    cout << "--------------------------------------------";
-   cout << endl << "ANSI String" << endl;
+   cout << endl
+        << "ANSI String" << endl;
    PrintAsAscii(param);
 }
 
 void PrintLeaseTime(DHCPCAPI_PARAMS param)
 {
    /* Print the Lease Time
-   * This is stored as Option 51
-   *
-   *
-   */
-   cout << endl << "\nLEASE TIME" << endl;
+    * This is stored as Option 51
+    *
+    *
+    */
+   cout << endl
+        << "\nLEASE TIME" << endl;
    cout << "--------------------------------------------" << endl;
    PrintAsInt(param);
    cout << endl;
@@ -276,75 +275,75 @@ void PrintLeaseTime(DHCPCAPI_PARAMS param)
 void PrintMessageType(DHCPCAPI_PARAMS param)
 {
    /* Print the message type
-   * This is stored as Option 53
-   *
-   *
-   */
-   cout << endl << "\nMESSAGE TYPE" << endl;
-   cout << "--------------------------------------------" << endl;;
+    * This is stored as Option 53
+    *
+    *
+    */
+   cout << endl
+        << "\nMESSAGE TYPE" << endl;
+   cout << "--------------------------------------------" << endl;
+   ;
    PrintAsInt(param);
 }
 
 void PrintServerIP(DHCPCAPI_PARAMS param)
 {
    /* Print the DHCP servers IP address
-   * This is stored as Option 54
-   *
-   *
-   */
-   cout << endl << "\nDHCP SERVER" << endl;
+    * This is stored as Option 54
+    *
+    *
+    */
+   cout << endl
+        << "\nDHCP SERVER" << endl;
    cout << "--------------------------------------------";
    PrintAsIP(param);
-} 
-void Printer(char input[500]){
-    cout << input << endl;
+}
+void Printer(char input[500])
+{
+   cout << input << endl;
 }
 
+int main()
+{
 
+   struct DHCPAPI_PARAMS
+   {
+      ULONG Flags;
+      ULONG OptionId;
+      BOOL IsVendor;
+      LPBYTE Data;
+      DWORD nBytesData;
+   };
+   /*
+   DHCPCAPI_PARAMS param;
+   param.Flags = 0;
+   param.OptionId = 43;
+   param.IsVendor = true;
+   //param.Data
+   PrintAsIP(param);
+   */
+   DHCPCAPI_PARAMS param = {0,
+                            0,
+                            FALSE,
+                            NULL,
+                            0};
 
+   int x = 1;
+   char name[26];
+   char hold[26];
+   while (x != 0)
+   {
+      // cout<<"Hello World"<< endl;
+      // cin>>name;
+      cin >> x;
+      param.OptionId = x;
+      PrintMessageType(param);
+      PrintServerIP(param);
+      PrintLeaseTime(param);
+      PrintVendor(param);
+      PrintDNS(param);
+      Printer("\n\nEnter New Value:");
+   }
 
-
-
-int main(){
-
-    struct DHCPAPI_PARAMS {
-    ULONG  Flags;
-    ULONG  OptionId;
-    BOOL   IsVendor;
-    LPBYTE Data;
-    DWORD  nBytesData;
-    };
-    /*
-    DHCPCAPI_PARAMS param;
-    param.Flags = 0;
-    param.OptionId = 43;
-    param.IsVendor = true;
-    //param.Data
-    PrintAsIP(param);
-    */
-    DHCPCAPI_PARAMS param = {0,
-                             0,
-                             FALSE,
-                             NULL,
-                             0
-                              };
-
-
-    int x = 1;
-    char name[26];char hold[26];
-    while(x !=0){
-    //cout<<"Hello World"<< endl;
-    //cin>>name;    
-    cin>>x;
-    param.OptionId = x;
-    PrintMessageType(param);
-    PrintServerIP(param);
-    PrintLeaseTime(param);
-    PrintVendor(param);
-    PrintDNS(param);
-    Printer("\n\nEnter New Value:");
-    }
-
-    return 0;
-
+   return 0;
 }
--- DNS/simpleDNS/code.c
+++ DNS/simpleDNS/code.c
@@ -1,148 +1,141 @@
 typedef struct
 {
-unsigned short id;       // identification number
-unsigned char rd :1;     // recursion desired
-unsigned char tc :1;     // truncated message
-unsigned char aa :1;     // authoritive answer
-unsigned char opcode :4; // purpose of message
-unsigned char qr :1;     // query/response flag
-unsigned char rcode :4;  // response code
-unsigned char cd :1;     // checking disabled
-unsigned char ad :1;     // authenticated data
-unsigned char z :1;      // its z! reserved
-unsigned char ra :1;     // recursion available
-unsigned short q_count;  // number of question entries
-unsigned short ans_count; // number of answer entries
-unsigned short auth_count; // number of authority entries
-unsigned short add_count; // number of resource entries
+    unsigned short id;         // identification number
+    unsigned char rd : 1;      // recursion desired
+    unsigned char tc : 1;      // truncated message
+    unsigned char aa : 1;      // authoritive answer
+    unsigned char opcode : 4;  // purpose of message
+    unsigned char qr : 1;      // query/response flag
+    unsigned char rcode : 4;   // response code
+    unsigned char cd : 1;      // checking disabled
+    unsigned char ad : 1;      // authenticated data
+    unsigned char z : 1;       // its z! reserved
+    unsigned char ra : 1;      // recursion available
+    unsigned short q_count;    // number of question entries
+    unsigned short ans_count;  // number of answer entries
+    unsigned short auth_count; // number of authority entries
+    unsigned short add_count;  // number of resource entries
 } DNS_HEADER;
 
-
-Structure for the query ( we wont keep the name in this structure since size
-is variable)
+// Structure for the query ( we wont keep the name in this structure since size is variable)
 
 typedef struct
 {
-unsigned short qtype;
-unsigned short qclass;
+    unsigned short qtype;
+    unsigned short qclass;
 } QUESTION;
-Resource Record
+// Resource Record
 
-
-typedef struct
+    typedef struct
 {
-unsigned short type;
-unsigned short _class;
-unsigned int ttl;
-unsigned short data_len;
+    unsigned short type;
+    unsigned short _class;
+    unsigned int ttl;
+    unsigned short data_len;
 } R_DATA;
-Once again name and rdata have been kept out.
-These two structures will help
+// Once again name and rdata have been kept out.These two structures will help
 
-
-typedef struct
+    typedef struct
 {
-unsigned char *name;
-R_DATA *resource;
-unsigned char *rdata;
+    unsigned char *name;
+    R_DATA *resource;
+    unsigned char *rdata;
 } RES_RECORD;
 
 typedef struct
 {
-unsigned char *name;
-QUESTION *ques;
+    unsigned char *name;
+    QUESTION *ques;
 } QUERY;
 
+int main()
+{
 
-int main(){
+    SOCKET s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // UDP packet for DNS queries
+    RES_RECORD answers[20], auth[20], addit[20];         // the replies from the DNS server
+    sockaddr_in dest;
 
-	SOCKET s=socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP); //UDP packet for DNS queries
-	RES_RECORD answers[20],auth[20],addit[20]; //the replies from the DNS server
-	sockaddr_in dest;
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(53);
+    dest.sin_addr.s_addr = inet_addr(dns_servers[0]); // use the first dns server
 
-	dest.sin_family=AF_INET;
-	dest.sin_port=htons(53);
-	dest.sin_addr.s_addr=inet_addr(dns_servers[0]); //use the first dns server
+    unsigned char buf[65536], *qname, *reader;
+    DNS_HEADER *dns = NULL;
+    QUESTION *qinfo = NULL;
 
-unsigned char buf[65536],*qname,*reader;
-DNS_HEADER *dns = NULL;
-QUESTION *qinfo = NULL;
+    // Set the DNS structure to standard queries
+    dns = (DNS_HEADER *)&buf;
 
-//Set the DNS structure to standard queries
-dns=(DNS_HEADER*)&buf;
+    // set up the header
+    dns->id = (unsigned short)htons(GetCurrentProcessId());
+    dns->qr = 0;     // This is a query
+    dns->opcode = 0; // This is a standard query
+    dns->aa = 0;     // Not Authoritative
+    dns->tc = 0;     // This message is not truncated
+    dns->rd = 1;     // Recursion Desired
+    dns->ra = 0;     // Recursion not available! hey we dont have it (lol)
+    dns->z = 0;
+    dns->ad = 0;
+    dns->cd = 0;
+    dns->rcode = 0;
+    dns->q_count = htons(1); // we have only 1 question
+    dns->ans_count = 0;
+    dns->auth_count = 0;
+    dns->add_count = 0;
 
-//set up the header
-dns->id = (unsigned short)htons(GetCurrentProcessId());
-dns->qr = 0; //This is a query
-dns->opcode = 0; //This is a standard query
-dns->aa = 0; //Not Authoritative
-dns->tc = 0; //This message is not truncated
-dns->rd = 1; //Recursion Desired
-dns->ra = 0; //Recursion not available! hey we dont have it (lol)
-dns->z = 0;
-dns->ad = 0;
-dns->cd = 0;
-dns->rcode = 0;
-dns->q_count = htons(1); //we have only 1 question
-dns->ans_count = 0;
-dns->auth_count = 0;
-dns->add_count = 0;
+    // point to the query portion
+    qname = (unsigned char *)&buf[sizeof(DNS_HEADER)];
+    ChangetoDnsNameFormat(qname, host);
 
-//point to the query portion
-qname =(unsigned char*)&buf[sizeof(DNS_HEADER)];
-ChangetoDnsNameFormat(qname,host);
+    qinfo = (QUESTION *)&buf[sizeof(DNS_HEADER) + (strlen((const char *)qname) + 1)];
 
-qinfo =(QUESTION*)&buf[sizeof(DNS_HEADER) + (strlen((const char*)qname) + 1)];
-
-//fill it
-qinfo->qtype = htons(1); //we are requesting the ipv4 address
-qinfo->qclass = htons(1); //its internet (lol)
+    // fill it
+    qinfo->qtype = htons(1);  // we are requesting the ipv4 address
+    qinfo->qclass = htons(1); // its internet (lol)
 
 sendto(s,(char*)buf,sizeof(DNS_HEADER) + (strlen((const char*)qname)+1) +
 sizeof(QUESTION),0,(sockaddr*)&dest,sizeof(dest))==SOCKET_ERROR)
 int i=sizeof(dest);
-recvfrom (s,(char*)buf,65536,0,(sockaddr*)&dest,&amp;i);
-dns=(DNS_HEADER*)buf;
+recvfrom(s, (char *)buf, 65536, 0, (sockaddr *)&dest, &amp; i);
+dns = (DNS_HEADER *)buf;
 
-//move ahead of the dns header and the query field
-reader=&buf[sizeof(DNS_HEADER) + (strlen((const char*)qname)+1) +
-sizeof(QUESTION)];
+// move ahead of the dns header and the query field
+reader = &buf[sizeof(DNS_HEADER) + (strlen((const char *)qname) + 1) +
+              sizeof(QUESTION)];
 
 printf("nThe response contains : ");
-printf("n %d Questions.",ntohs(dns->q_count));
-printf("n %d Answers.",ntohs(dns->ans_count));
-printf("n %d Authoritative Servers.",ntohs(dns->auth_count));
-printf("n %d Additional records.nn",ntohs(dns->add_count));
+printf("n %d Questions.", ntohs(dns->q_count));
+printf("n %d Answers.", ntohs(dns->ans_count));
+printf("n %d Authoritative Servers.", ntohs(dns->auth_count));
+printf("n %d Additional records.nn", ntohs(dns->add_count));
 
-//reading answers
-int stop=0;
-for(i=0;i<ntohs(dns->ans_count);i++)
+// reading answers
+int stop = 0;
+for (i = 0; i < ntohs(dns->ans_count); i++)
 {
-answers[i].name=ReadName(reader,buf,stop);
-reader+=stop;
-answers[i].resource=(R_DATA*)(reader);
-reader+=sizeof(R_DATA);
-if(ntohs(answers[i].resource->type)==1)
-{
-    answers[i].rdata=new unsigned char[ntohs(answers[i].resource->data_len)];
-    for(int j=0;j<ntohs(answers[i].resource->data_len);j++)
-        answers[i].rdata[j]=reader[j];
-    answers[i].rdata[ntohs(answers[i].resource->data_len)]='';
-    reader+=ntohs(answers[i].resource->data_len);
+    answers[i].name = ReadName(reader, buf, stop);
+    reader += stop;
+    answers[i].resource = (R_DATA *)(reader);
+    reader += sizeof(R_DATA);
+    if (ntohs(answers[i].resource->type) == 1)
+    {
+        answers[i].rdata = new unsigned char[ntohs(answers[i].resource->data_len)];
+        for (int j = 0; j < ntohs(answers[i].resource->data_len); j++)
+            answers[i].rdata[j] = reader[j];
+        answers[i].rdata[ntohs(answers[i].resource->data_len)] ='';
+        reader += ntohs(answers[i].resource->data_len);
+    }
+    else
+    {
+        answers[i].rdata = ReadName(reader, buf, stop);
+        reader += stop;
+    }
 }
-else
-{
-    answers[i].rdata=ReadName(reader,buf,stop);
-    reader+=stop;
-}
-}
-and so on….
-The IP address in the rdata section will be as numbers which must be converted
-to a string and then to the dotted format using inet_ntoa like this
+// and so on….The IP address in the rdata section will be as numbers which must be converted  to a string and then to the dotted format using inet_ntoa like this
 
-sockaddr_in a;
-long *p;
-p=(long*)addit[i].rdata;
-a.sin_addr.s_addr=(*p);
-printf("has IPv4 address : %s",inet_ntoa(a.sin_addr));
+    sockaddr_in a;
+    long *p;
+    p = (long *)addit[i].rdata;
+    a.sin_addr.s_addr = (*p);
+    printf("has IPv4 address : %s", inet_ntoa(a.sin_addr));
 }
